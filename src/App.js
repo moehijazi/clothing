934b150485d85ad9695a69b8452a9e843d9657f8
Mohamad Hijazi <hijazimohamad@hotmail.com>
2019-08-08 13:43:48 +0300

    added routing to menu items

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import './App.css';
 import Homepage from './Pages/Homepage/Homepage';
+import { Switch,Route } from 'react-router-dom';
 
+const HatsPage = () =>(
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+)
 
 function App() {
   return (
     <div >
-      <Homepage /> 
+      <Route exact path='/' component= {Homepage} />
+      <Route path='/hats' component = {HatsPage} /> 
     </div>
   );
 }
